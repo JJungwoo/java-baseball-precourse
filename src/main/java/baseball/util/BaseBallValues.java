e178bde6e41f46147d50cce9b1044251c43a2259
JJungwoo <jungwoo5759@gmail.com>
2022-02-20 01:25:17 +0900

    feature: 야구 게임 공통 값 추가

--- src/main/java/baseball/util/BaseBallValues.java
+++ src/main/java/baseball/util/BaseBallValues.java
@@ -2,6 +2,8 @@ package baseball.util;
 
 public class BaseBallValues {
 
+    public static final int GAME_END_SIGNAL = 1;
+
     public static final int BEGIN_NUMBER = 1;
     public static final int END_NUMBER = 9;
 
